bd4f42a53bac4dfca238d803434507408c85d6a5
Catermatics AB <catermatics@gmail.com>
2025-01-08 19:51:34 +0100

    Update import serial Test.py
    This version work with both Ultrasonic and Digital pin control

--- import serial Test.py
+++ import serial Test.py
@@ -1,59 +1,82 @@
-#Test Sync 123
-# pip install pymata4
-# pip install pyserial
-
-# Import the necessary libraries
-import time
+import sys
+from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButton 
+import serial
 from pymata4 import pymata4
-import tkinter
-Top = tkinter.Tk()
-Top.title("Distance Sensor test")
 
+class MainDialog(QDialog):
+    def __init__(self):
+        super().__init__()
 
+        self.setWindowTitle("Main Dialog")
+        self.setGeometry(100, 100, 300, 200)
 
+        # Set background picture and scale to fit the dialog size
+        self.setStyleSheet("""
+            QDialog {
+            background-image: url('C:/Work Folder/CATERMATICS AB/Background picture 1.jpg');
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: cover;
+            }
+        """)
 
-# Define the pins
-triggerPin = 10
-echo_pin = 11
-pins_green = 7
-num_steps=512
+        layout = QVBoxLayout()
 
-# Create a pymata4 instance
-board = pymata4.Pymata4()
+        self.label = QLabel("Distance:")
+        layout.addWidget(self.label)
 
-# Define the callback function
-def the_callback(data):
-    print("Distance: ", data[2])
+        self.dist_textbox = QLineEdit(self)
+        self.dist_textbox.setText("0")
+        layout.addWidget(self.dist_textbox)
 
-# Set the pin mode
-board.set_pin_mode_sonar(triggerPin, echo_pin, the_callback)
-board.set_pin_mode_digital_output(pins_green)
+        self.update_button = QPushButton("Update", self)
+        self.update_button.clicked.connect(self.update_distance)
+        layout.addWidget(self.update_button)
 
+        self.RedLED_button_ON = QPushButton("Red LED On", self)
+        self.RedLED_button_ON.clicked.connect(self.RED_LED_ON)
+        layout.addWidget(self.RedLED_button_ON)
 
-while True:
-    try:
-        board.sonar_read(triggerPin)
-        time.sleep(1)
-    except Exception:
-        board.shutdown()
-        break
+        self.RedLED_button_OFF = QPushButton("Red LED Off", self)
+        self.RedLED_button_OFF.clicked.connect(self.RED_LED_OFF)
+        layout.addWidget(self.RedLED_button_OFF)
 
+        self.setLayout(layout)
 
-while True:
-    print('ON')
-    board.digital_write(pins_green, 1)
-    time.sleep(2)
-    print('OFF')
-    board.digital_write(pins_green, 0)
-    board.sonar_read(triggerPin)
-    time.sleep(2)    
-    
+        # Initialize the Pymata4 board
+        self.board = pymata4.Pymata4()
 
+        # Set up the ultrasonic sensor
+        self.trigger_pin = 10
+        self.echo_pin = 11
+        self.LED_pin = 12
+        self.board.set_pin_mode_sonar(self.trigger_pin, self.echo_pin, self.callback)
+        self.board.set_pin_mode_digital_output(self.LED_pin)
 
+        self.distance = 0
 
+    def callback(self, data):
+        self.distance = data[2]
 
+    def update_distance(self):
+        # Measure the distance using the ultrasonic sensor
+        self.board.sonar_read(self.trigger_pin)
+        self.dist_textbox.setText(str(self.distance))
+        print(f"Distance updated to: {self.distance}")
 
+    def RED_LED_ON(self):
+        self.board.digital_write(self.LED_pin, 1)
+        print("Red LED ON")
+
+    def RED_LED_OFF(self):
+        self.board.digital_write(self.LED_pin, 0)
+        print("Red LED OFF", self.board.digital_read(2))
         
 
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    dialog = MainDialog()
+    dialog.show()
+    sys.exit(app.exec_())
 
-    
+
